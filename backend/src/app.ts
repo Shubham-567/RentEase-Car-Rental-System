7918ed5b087020de08b5762831f154e2ee632437
Shubham <shubhampatil1356@gmail.com>
2025-07-16 02:05:13 +0530

    Updated cors config for frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,28 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/router.js";
 
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://rent-ease-site.vercel.app/", // deployed frontend
+];
+
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", router);
